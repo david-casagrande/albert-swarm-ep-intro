c8c00bf76e23734791eff2c058795a7b883aae8b
David Casagrande <dncasagrande@gmail.com>
2014-01-31 01:26:33 -0500

    checking express env

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ var server = http.createServer(app);
 reload(server, app);
 
 var port = Number(process.env.PORT || 5000);
-console.log(process.env.PORT);
+console.log(app.settings.env);
 server.listen(port, function(){
 	console.log('Server is now running on port '+ port);
 });
